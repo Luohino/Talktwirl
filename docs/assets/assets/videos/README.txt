f435e3719dbaf22e9650f38916d9b538a0f06f0b
Luohino <aniketsingh821305@gmail.com>
2025-07-26 19:42:41 +0530

    Remove all files from master branch

--- docs/assets/assets/videos/README.txt
+++
@@ -1,1 +0,0 @@
-Place royalty-free, open-license MP4 videos here for use in the TwirlScreen demo feed. Example filenames: twirl1.mp4, twirl2.mp4, etc.
--- lib/core/inbox_notifier.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class InboxRefreshNotifier extends ValueNotifier<int> {
-  InboxRefreshNotifier() : super(0);
-  void refresh() => value++;
-}
-
-final inboxRefreshNotifier = InboxRefreshNotifier(); 
